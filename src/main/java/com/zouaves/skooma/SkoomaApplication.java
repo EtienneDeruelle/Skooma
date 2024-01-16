38cb308e94143397d225c4f1cc7db81ddbaf57b7
Etienne DERUELLE <etienne.deruelle@equasens.com>
2024-01-16 08:42:35 +0100

    initial commit

---
+++ src/main/java/com/zouaves/skooma/SkoomaApplication.java
@@ -0,0 +1,13 @@
+package com.zouaves.skooma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkoomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkoomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zouaves/skooma/SkoomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zouaves.skooma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkoomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
